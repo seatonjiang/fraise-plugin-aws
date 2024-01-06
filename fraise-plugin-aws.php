d3e374e9b06ad50592577f77099fc4d7a8bf9914
Seaton Jiang <hi@seatonjiang.com>
2024-01-06 17:20:37 +0800

    feat: add plugin name file

---
+++ fraise-plugin-aws.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Fraise - 对象存储工具包
+ * Description: 使用分布式存储服务所需的工具包，具有高扩展性、低成本、可靠安全等优点。
+ * Version: 2024.01.06
+ * Plugin URI: https://github.com/seatonjiang/fraise-plugin-aws
+ * Author: Seaton Jiang
+ * Author URI: https://seatonjiang.com
+ * License: MIT License
+ * License URI: https://github.com/seatonjiang/fraise-plugin-aws/blob/main/LICENSE
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!has_action('fraise-plugin-aws')) {
+    add_action('fraise-plugin-aws', function () {
+        require_once dirname(__FILE__) . '/vendor/autoload.php';
+    });
+}
